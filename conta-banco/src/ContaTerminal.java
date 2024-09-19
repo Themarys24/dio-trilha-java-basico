c1bc76bdcc6db6a0b9b1af1c219ab81272f3a443
Maria Ferreira <maria.ferreira312017@gmail.com>
2024-09-19 21:15:02 +0800

    Projeto feito

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int number ;
+        String agency ;
+        String customerName ;
+        double balance = 237.48 ;
+
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("Please enter the agency number:  ");
+        agency = scan.next();
+
+        System.out.println("Please enter the user number:  ");
+        number = scan.nextInt();
+
+        System.out.println("Please enter the Customer Name:  ");
+        customerName = scan.next();
+
+        System.out.println("Hello " + customerName + ", thanks you for creating an account with our bank, your Agency is: " + agency +
+         ", Account: " + number + " and your balance: " + balance + " is now available for withdrawal");
+
+
     }
 }
